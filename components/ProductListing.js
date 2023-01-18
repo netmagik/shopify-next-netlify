b4a1e85408e178b8e7b875071169e00262acfd6a
Irina <irina@netmagik.com>
2023-01-17 20:56:26 -0500

    Add and style product pricing

--- components/ProductListing.js
+++ components/ProductListing.js
@@ -1,7 +1,9 @@
 import Link from 'next/link';
+import { formatPrice } from "../utilityFunctions";
 
 export default function ProductListing({ product }) {
   let image = product.images.edges[0].node;
+  console.log({product})
   return (
     <li className="product-card">
       <div className="product-card-frame">
@@ -11,6 +13,7 @@ export default function ProductListing({ product }) {
       </div>
       <div className="product-card-text">
         <h3 className="product-card-title">{product.title}</h3>
+        <p className="product-card-price">{formatPrice(product.priceRange.minVariantPrice.amount)}</p>
         <p className="product-card-description">
           {product.description.substring(0, 60)}...
         </p>
